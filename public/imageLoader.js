63a6cf762388fe706c378b589dd5831092a275ae
antyneskul <antyneskul@gmail.com>
2018-10-10 12:00:40 +0200

    experemntal brightness detection

--- public/imageLoader.js
+++ public/imageLoader.js
@@ -11,10 +11,31 @@ img.addEventListener('load', function () {
   canvas.height = img.height;
   context.drawImage(img, 0, 0, img.width, img.height);
 
-  const data = canvas.toDataURL('image/png');
+  const dataUrl = canvas.toDataURL('image/png');
+
+  const imageData = context.getImageData(0,0,canvas.width,canvas.height);
+  const data = imageData.data;
+
+  let r,g,b,avg;
+  let colorSum = 0;
+
+  for(let x = 0, len = data.length; x < len; x+=4) {
+      r = data[x];
+      g = data[x+1];
+      b = data[x+2];
+
+      avg = Math.floor((r+g+b)/3);
+      colorSum += avg;
+  }
+
+
+
+  const brightness = Math.floor(colorSum / (img.width * img.height));
+
+  localStorage.setItem('brightness', brightness);
 
   try {
-    localStorage.setItem('img', data);
+    localStorage.setItem('img', dataUrl);
   } catch (e) {
     console.log('Image is too big')
   }
--- src/componets/Background/Hexagon.js
+++ src/componets/Background/Hexagon.js
@@ -2,8 +2,14 @@ import React from 'react';
 import './Hexagon.css'
 
 const Hexagon = props => {
+    const getBrightness = () => localStorage.getItem('brightness');
+    const getBrightnessClassName = () => {
+        const brightness = getBrightness();
+
+        return (brightness && Number(brightness) > 180) ? 'light' : '';
+    }
     return (
-        <div className={'Hexagon'}>
+        <div className={`Hexagon ${getBrightnessClassName()}`}>
             {props.children}
         </div>
     )
